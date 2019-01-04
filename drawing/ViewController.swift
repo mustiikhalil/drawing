ee51634acb06dd3b71294faa1f95d685632ebd61
mustii <mustii@mmk.one>
2019-01-04 17:22:59 +0300

    Small fun thing

--- drawing/ViewController.swift
+++ drawing/ViewController.swift
@@ -8,13 +8,63 @@
 
 import UIKit
 
+class Canvas: UIView {
+ 
+    var lines: [[CGPoint]] = []
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setLineWidth(10)
+        context.setLineCap(.butt)
+        
+        lines.forEach { (line) in
+            for (i, p) in line.enumerated() {
+                if i == 0 {
+                    context.move(to: p)
+                } else {
+                    context.addLine(to: p)
+                }
+            }
+        }
+        
+        context.strokePath()
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([])
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let point = touches.first?.location(in: nil) else { return }
+        
+        guard var line = lines.popLast() else { return }
+        line.append(point)
+        lines.append(line)
+        
+        setNeedsDisplay()
+    }
+}
+
 class ViewController: UIViewController {
 
+    let canvas = Canvas()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        canvas.translatesAutoresizingMaskIntoConstraints = false
+        canvas.backgroundColor = .white
+        view.addSubview(canvas)
+        NSLayoutConstraint.activate([
+            canvas.topAnchor.constraint(equalTo: view.topAnchor),
+            canvas.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            canvas.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            canvas.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            ])
     }
-
-
 }
 
